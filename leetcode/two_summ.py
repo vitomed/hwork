5cd5eb788397a91816c8f28013db025177ea5156
vitomed <mediankin.viktor@gmail.com>
2026-05-16 17:43:40 +0300

    notes

---
+++ leetcode/two_summ.py
@@ -0,0 +1,16 @@
+def two_sum(nums: list[int], target: int, n=None) -> list[int]:
+        store = set()
+        store =
+
+
+"""
+Input:  nums = [2, 7, 11, 15], target = 9
+Output: [0, 1]
+Explanation: nums[0] + nums[1] = 2 + 7 = 9
+"""
+
+nums = [2, 7, 11, 15]
+target = 9
+output = [0, 1]
+
+assert output == two_sum()
